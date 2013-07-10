0849857e9775acc30dae472a3094562fcdb2ebbe
mhazegh <mhazegh@gmail.com>
2013-07-09 19:04:21 -0600

    Added ability to search by actor.

--- movie_sorter.py
+++ movie_sorter.py
@@ -32,18 +32,51 @@ def main():
                         choices=['critics_score',
                                  'audience_score'],
                         help='Which type of score to rank by.')
+    parser.add_argument('-a',
+                        '--actor',
+                        help='Search for movies with a specific actor.')
+    parser.add_argument('-u',
+                        '--update',
+                        action='store_true',
+                        default=False,
+                        help='Update the json file.')
+    parser.add_argument('-r',
+                        '--rebuild',
+                        action='store_true',
+                        default=False,
+                        help='Completely rebuild the json file.')
     args = parser.parse_args()
 
-    if 'movie_data.json' in os.listdir('.'):
-        movie_dict = json.load(open('movie_data.json', 'r'))
-        sort_movies(movie_dict, args.genre, args.score_type)
+    if args.update or args.rebuild:
+        movie_dict = build_dict(args.directory, args.rebuild)
     else:
-        movie_dict = build_dict(args.directory)
-        sort_movies(movie_dict, args.genre, args.score_type)
+        try:
+            movie_dict = json.load(open('movie_data.json', 'r'))
+        except IOError:
+            print ('Error: movie_data.json not found in current directory. '
+                   'Build it by using the -u option.')
+            return
+        sort_movies(movie_dict, args.genre, args.score_type, args.actor)
 
 
-def sort_movies(movie_dict, genre='All', score_type='critics_score'):
-    if genre == 'All':
+def sort_movies(movie_dict, genre='All',
+                score_type='critics_score',
+                actor=None):
+    if actor:
+        matching_movies = []
+        for movie in sorted(movie_dict.items(),
+                            key=lambda (k, v): v[score_type],
+                            reverse=True):
+            if actor.lower() in [a.lower() for a in movie[1]['actors']]:
+                matching_movies.append((movie[1]['title'],
+                                        movie[1][score_type]))
+        print '-'*50
+        print actor
+        print '-'*50
+        for tup in matching_movies:
+            print '\t {0} {1}'.format(tup[0], tup[1])
+
+    elif genre == 'All':
         # For each genre, print out movies that belong to that genre
         # and their scores.
         action_and_adventure = []
@@ -159,58 +192,61 @@ def sort_movies(movie_dict, genre='All', score_type='critics_score'):
             print '\t {0} {1}'.format(tup[0], tup[1])
 
 
-def build_dict(directory='.'):
-    movie_dict = {}
+def build_dict(directory='.', rebuild=False):
+    if rebuild:
+        movie_dict = {}
+        write_flag = 'w'
+    else:
+        try:
+            movie_dict = json.load(open('movie_data.json', 'r'))
+        except IOError:
+            movie_dict = {}
+        write_flag = 'wa'
 
     # Get a list of movies in the specified directory.
     white_list = ('.avi', '.mp4')
     movies = [os.path.splitext(m)[0] for m
               in open('test_movies.txt', 'r').read().splitlines()
               if m.endswith(white_list)]
+    movies = [m for m in movies if m not in movie_dict]
     key = 'jjzq2ekfmxjzymr6qqc3x47e'
 
     # Build a dictionary of titles, scores, and ids.
-    for movie in movies:
-        if movie not in movie_dict:
-            cleaned_movie = movie.replace(' ', '+')
-            url = ('http://api.rottentomatoes.com/api/'
-                   'public/v1.0/movies.json?apikey={0}'
-                   '&q={1}&page_limit=1'.format(key, cleaned_movie))
-            try:
-                response = urllib2.urlopen(url)
-                data = json.load(response)
-                id = data['movies'][0]['id']
-                title = data['movies'][0]['title']
-                critics_score = data['movies'][0]['ratings']['critics_score']
-                audience_score = data['movies'][0]['ratings']['audience_score']
-                movie_dict[movie] = {'id': id,
-                                     'title': title,
-                                     'critics_score': critics_score,
-                                     'audience_score': audience_score}
-            except IndexError:
-                print 'Could not find {}'.format(movie)
-            except urllib2.URLError:
-                print 'Error fectching data for {}'.format(movie)
-
-    # Loop through the movies again to get their genre.
-    # We have to use the 'id' found in the previous query to
-    # get the genre of a movie.
-    for movie in movie_dict:
+    for num, movie in enumerate(movies):
+        print '{} of {} - {}'.format(num, len(movies), movie)
+        cleaned_movie = movie.replace(' ', '+')
         url = ('http://api.rottentomatoes.com/api/'
-               'public/v1.0/movies/'
-               '{0}.json?apikey={1}'.format(movie_dict[movie]['id'], key))
+               'public/v1.0/movies.json?apikey={0}'
+               '&q={1}&page_limit=1'.format(key, cleaned_movie))
         try:
+            response = urllib2.urlopen(url)
+            data = json.load(response)
+            id = data['movies'][0]['id']
+            title = data['movies'][0]['title']
+            critics_score = data['movies'][0]['ratings']['critics_score']
+            audience_score = data['movies'][0]['ratings']['audience_score']
+            actors = [a['name'] for a in data['movies'][0]['abridged_cast']]
+            movie_dict[movie] = {'id': id,
+                                 'title': title,
+                                 'critics_score': critics_score,
+                                 'audience_score': audience_score,
+                                 'actors': actors}
+            # We have to use the 'id' found in the previous query to
+            # get the genre of a movie.
+            url = ('http://api.rottentomatoes.com/api/'
+                   'public/v1.0/movies/'
+                   '{0}.json?apikey={1}'.format(id, key))
             response = urllib2.urlopen(url)
             data = json.load(response)
             genres = data['genres']
             movie_dict[movie]['genres'] = genres
         except IndexError:
-            print 'Could not get genre for {}'.format(movie)
+            print 'Could not find {}'.format(movie)
         except urllib2.URLError:
             print 'Error fectching data for {}'.format(movie)
 
     # Dump our updated movie dictionary to a .json file.
-    with open('movie_data.json', 'wa') as fp:
+    with open('movie_data.json', write_flag) as fp:
         json.dump(movie_dict, fp, indent=4)
 
     return movie_dict
